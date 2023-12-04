1ce2c58d73d69949050c2f8560eeaccb77ce6c40
LehaBiceps <alexey005v@gmail.com>
2023-12-04 07:41:20 +0700

    1.1

--- Documents/Git/C#/Практика 1/1.7/1.7/Program.cs
+++ Documents/Git/C#/Практика 1/1.7/1.7/Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 public class Program
 {
-    static string path = "/Users/aleksej/Documents/Учёба/C#/Практика 1/1.7/task.json";
-
     public class TaskJson
     {
         public string taskName { get; set; }
@@ -11,15 +11,10 @@ public class Program
         public DateTime taskDate { get; set; }
     }
 
-    static void showTask(List<Task> task)
-    {
-        foreach (TaskJson info in task)
-        {
-            Console.WriteLine($"Задача: {info.taskName}\nописание: {info.taskDescription}\nдата: {info.taskDate}\n");
-        }
-    }
+    static string path = "/Users/aleksej/Documents/Учёба/C#/Практика 1/1.7/task.json";
+    static List<TaskJson> task = new List<TaskJson>();
 
-    static void addTask(List<Task> task)
+    static void addTask(List<TaskJson> task)
     {
         Console.WriteLine("Введите название задачи: ");
         string name = Console.ReadLine();
@@ -30,25 +25,75 @@ public class Program
         Console.WriteLine("Введите дату окончания: ");
         DateTime time = DateTime.Parse(Console.ReadLine());
 
-        Task add = new Task { taskName = name, Description = description, deadline = deadline };
-        task.Add();
-        Console.WriteLine("Список обновлён");
+        TaskJson jsonRecord = new TaskJson() { taskName = name, taskDescription = description, taskDate = time };
+        task.Add(jsonRecord);
     }
 
-    static void deleteTask(List<Task> task)
+    //static void deleteTask(List<TaskJson> task)
+    //{
+    //    int number = 1;
+
+    //    foreach (TaskJson list in task)
+    //    {
+    //        Console.WriteLine(number + " - " + list);
+    //        number++;
+    //    }
+
+    //    Console.WriteLine("Введите номер для удаления:");
+    //    int choiseDelete = int.Parse(Console.ReadLine());
+
+    //    if (choiseDelete > 0 && choiseDelete < task.Count)
+    //    {
+    //        task.RemoveAt(choiseDelete);
+    //        Console.WriteLine("Задача удалена");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Неверный номер задачи");
+    //    }
+    //}
+
+    //static void editTask()
+    //{
+
+    //}
+
+    static void showTask(List<TaskJson> task)
     {
-
+        foreach (var list in task)
+        {
+            Console.WriteLine($"Задача: {list.taskName}\tОписание: {list.taskDescription}\tОкончание: {list.taskDate}");
+        }
     }
 
-    static void editTask(List<Task> task)
+    static void saveTask()
     {
-
+        using (StreamWriter file = File.CreateText(path))
+        {
+            var serializer = new Newtonsoft.Json.JsonSerializer();
+            serializer.Serialize(file, task);
+        }
     }
+
+    static void loadTask()
+    {
+        if (File.Exists(path))
+        {
+            using (StreamReader file = File.OpenText(path))
+            {
+                var serializer = new Newtonsoft.Json.JsonSerializer();
+                task = (List<TaskJson>)serializer.Deserialize(file, typeof(List<TaskJson>));
+            }
+        }
+    }
+
 
 
 
     public static void Main()
     {
+        loadTask();
+
         Console.WriteLine("Привет! Выбери действие: ");
         Console.Write("1 - посмотреть список дел\n2 - добавить задачу\n3 - удалить задачу\n4 - редактировать задачи\nВвод: ");
         int firstСhoice = int.Parse(Console.ReadLine());
@@ -58,254 +103,28 @@ public class Program
         {
             case 1:
                 Console.WriteLine("\nВыбрано: посмотреть список дел");
-                Console.Write("1 - на сегодня\n2 - на завтра\n3 - неделю\n4 - за всё время\n5 - предстоящие\n6 - выполненные\nВвод: ");
+                showTask(task);
                 secondChoice = int.Parse(Console.ReadLine());
                 break;
             case 2:
                 Console.WriteLine("\nВыбрано: добавить задачу");
+                addTask(task);
+                Console.WriteLine("Список обновлён");
                 break;
             case 3:
                 Console.WriteLine("Выбрано: удалить задачу\n");
+                ///
+                Console.WriteLine("Список обновлён");
                 break;
             case 4:
                 Console.WriteLine("Выбрано: редактировать задачи\n");
+                ///
+                Console.WriteLine("Список обновлён");
                 break;
         }
 
-        switch (secondChoice)
-        {
-            case 1:
-                Console.WriteLine("Выбрано: на сегодня\n");
-                break;
-            case 2:
-                Console.WriteLine("Выбрано: на завтра\n");
-                break;
-            case 3:
-                Console.WriteLine("Выбрано: на неделю\n");
-                break;
-            case 4:
-                Console.WriteLine("Выбрано: за всё время\n");
-                break;
-            case 5:
-                Console.WriteLine("Выбрано: предстоящие\n");
-                break;
-            case 6:
-                Console.WriteLine("Выбрано: выполненные\n");
-                break;
-        }
+        saveTask();
     }
 }
 
-
-
-using System;
-using System.IO;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
-
-using static ConsoleApp1.Program2;
-
-
-namespace ConsoleApp1;
-
-public class Program2
-{
-    class Task
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime deadline { get; set; }
-    }
-
-   
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Это ваш ежедневник");
-
-        Console.WriteLine("Выберите действие, которое хотите совершить");
-        Console.WriteLine("1. Добавить задачу");
-        Console.WriteLine("2. Удалить задачу");
-        Console.WriteLine("3. Изменить задачу ");
-        Console.WriteLine("4. Показать все задачи");
-        Console.WriteLine("5. Показать задачи на сегодня");
-        Console.WriteLine("6. Показать задачи на завтра");
-        Console.WriteLine("7. Показать задачи на неделю");
-        Console.WriteLine("8. Показать просроченные задачи");
-        Console.WriteLine("9. Показать актульные задачи");
-        Console.WriteLine("q - выход из ежедневника");
-
-        string userInput = Console.ReadLine();
-
-        while (userInput != "q")
-        {
-            switch (userInput)
-            {
-                case "1":
-                    addTask(tasks);
-                    break;
-                case "2":
-                    deleteTask(tasks);
-                    break;
-                case "3":
-                    editTask(tasks);
-                    break;
-                case "4":
-                    showTask(tasks, DateTime.Now);
-                    break;
-                case "5":
-                    showTask(tasks, DateTime.Now.AddDays(1));
-                    break;
-                case "6":
-                    showTask(tasks, DateTime.Now.AddDays(7));
-                    break;
-                case "8":
-                    compliteTask();
-                    break;
-                case "9":
-                    uncompliteTask();
-                    break;
-            }
-
-            Console.WriteLine("Выберите действие, которое хотите совершить");
-            Console.WriteLine("1. Добавить задачу");
-            Console.WriteLine("2. Удалить задачу");
-            Console.WriteLine("3. Изменить задачу ");
-            Console.WriteLine("4. Показать все задачи");
-            Console.WriteLine("5. Показать задачи на сегодня");
-            Console.WriteLine("6. Показать задачи на завтра");
-            Console.WriteLine("7. Показать задачи на неделю");
-            Console.WriteLine("8. Показать просроченные задачи");
-            Console.WriteLine("9. Показать актульные задачи");
-            Console.WriteLine("q - выход из ежедневника");
-
-            userInput = Console.ReadLine();
-            saveTask();
-        }
-    }
-    static void addTask(List<Task> tasks)
-    {
-        Console.WriteLine("Введите название задачи");
-        string name = Console.ReadLine();
-        Console.WriteLine("Введите описание задачи");
-        string description = Console.ReadLine();
-        Console.WriteLine("Введите срок задачи");
-        Console.WriteLine("Введите день (дд):");
-        int day = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Введите месяц (мм):");
-        int month = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Введите год (гггг):");
-        int year = Convert.ToInt32(Console.ReadLine());
-
-        DateTime deadline = new DateTime(year, month, day);
-
-        Task task = new Task { Title = name, Description = description, deadline = deadline };
-
-        tasks.Add(task);
-        Console.WriteLine("Задача добавлена");
-    }
-    static void deleteTask(List<Task> tasks)
-    {
-        int index = 0;
-        foreach (Task task in tasks)
-        {
-            Console.WriteLine($"номер задачи - {index}, задача на этот день - {task} ");
-            index++;
-        }
-
-        Console.WriteLine("Введите номер задачи, чтобы удалить его");
-        int choose = Convert.ToInt32(Console.ReadLine());
-        if (choose >= 0 && choose < tasks.Count)
-        {
-            tasks.RemoveAt(choose);
-            Console.WriteLine("Задача удалена");
-        }
-        else
-        {
-            Console.WriteLine("Неверный номер задачи");
-        }
-    }
-    static void editTask(List<Task> tasks)
-    {
-        int index = 0;
-        foreach (Task t in tasks)
-        {
-            Console.WriteLine($"номер задачи - {index}, задача на этот день - {t.Title} описание {t.Description} ");
-            index++;
-        }
-        Console.WriteLine("Введите номер задачи, чтобы изменить его");
-        int choose = Convert.ToInt32(Console.ReadLine());
-        if (choose >= 0 && choose < tasks.Count)
-        {
-            tasks.RemoveAt(choose);
-        }
-        else
-        {
-            Console.WriteLine("Неверный номер задачи");
-        }
-
-        Console.WriteLine("Введите название задачи");
-        string name = Console.ReadLine();
-        Console.WriteLine("Введите описание задачи");
-        string description = Console.ReadLine();
-        Console.WriteLine("Введите срок задачи (дд:мм:гггг)");
-        Console.WriteLine("Введите день (дд):");
-        int day = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Введите месяц (мм):");
-        int month = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Введите год (гггг):");
-        int year = Convert.ToInt32(Console.ReadLine());
-
-        DateTime deadline = new DateTime(year, month, day);
-
-        var task = new Task { Title = name, Description = description, deadline = deadline };
-
-        tasks.Append(task);
-        Console.WriteLine("Задача изменена");
-    }
-    static void showTask(List<Task> tasks, DateTime time)
-    {
-        Console.WriteLine("Список задач");
-        foreach (Task t in tasks)
-        {
-            Console.WriteLine($"Название задачи: {t.Title}, описание: {t.Description}, дата: {t.deadline}");
-        }
-    }
-
-    static void compliteTask()
-    {
-        Console.WriteLine("Просроченные задачи:");
-
-        DateTime Time = DateTime.Now;
-
-        foreach (Task t in tasks)
-        {
-            if (t.deadline < Time)
-            {
-                Console.WriteLine($"Название: {t.Title}, Описание: {t.Description}, Дата завершения: {t.deadline}");
-            }
-        }
-    }
-    static void uncompliteTask()
-    {
-        Console.WriteLine("Актуальные задачи");
-        DateTime Time = DateTime.Now;
-
-        foreach (Task t in tasks)
-        {
-            if (t.deadline > Time)
-            {
-                Console.WriteLine($"Название: {t.Title}, Описание: {t.Description}, Дата завершения: {t.deadline}");
-            }
-        }
-    }
-    static void saveTask()
-    {
-        File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(tasks));
-    }
-}
 
--- Documents/Git/C#/Практика 1/1.8/1.8/Program.cs
+++ Documents/Git/C#/Практика 1/1.8/1.8/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using Newtonsoft.Json;
 
+public class Program
+{
+    static string API = "6bddde842f6a5bbd7fd3edc7fdcc9a88";
+    static string initial = "Томск";
+
+    public static void Main(string[] args)
+    {
+        weather(initial);
+        Console.WriteLine("Введите название города:");
+        string city = Console.ReadLine();
+
+        weather(city);
+    }
+
+    private static void weather(string city)
+    {
+        string adress = $"http://api.openweathermap.org/data/2.5/find?q={city}&type=like&APPID={API}\r\n";
+
+        using (WebClient client = new WebClient())
+        {
+            string load = client.DownloadString(adress);
+        }
+    }
+}
